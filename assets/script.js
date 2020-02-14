73b87313c25fe590087563426621d8b7dea4d6c4
Ivyparade <59892600+Ivyparade@users.noreply.github.com>
2020-02-13 21:42:07 -0500

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+
+$(".btn").click(function(){
+  console.log("Hi!");
+  console.log($("#citySearch").val());
+  toList();
+//   generateDash();
+  generateForecast();
+});
+
+function toList(){
+  $("#cityList").append("<div class='list-group-item'>" + $("#citySearch").val() + "</div>");
+};
+
+function generateForecast(){
+  var URL = "api.openweathermap.org/data/2.5/forecast?q=" + $("#citySearch") + "&appid=2a459ebf1649c4965d63c31cfc93309d";
+  $.ajax({
+    url: URL,
+    method: "GET"
+  }).then(function(response) {
+    response.forEach(function(){
+      console.log(response);
+    });
+  });
+};
